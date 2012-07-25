c6891a1a9591f97a7965ed3c9036f4f8900dcfa7
rjVapes <vapesk@gmail.com>
2012-07-25 16:40:14 -0400

    Initial Classes.java commit

---
+++ src/com/renderjunkies/noh/classes/Classes.java
@@ -0,0 +1,16 @@
+package com.renderjunkies.noh.classes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Classes extends JavaPlugin
+{
+	public void onEnable()
+	{
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+}
